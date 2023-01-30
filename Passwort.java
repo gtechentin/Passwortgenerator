64dd5f439107fb4e5142e387acdb2e4b43576dbb
g.techentin <g.techentin@polyas.de>
2023-01-30 12:45:42 +0100

    edited asciiFilter

--- Passwort.java
+++ Passwort.java
@@ -5,22 +5,21 @@ public class Passwort { //Java bedingte Formatierungen
         String ps = ""; 
         int i = 0; //Initialisieren der Variabeln
 
-        //Die "while-Schleife" läuft bis i < 10 und somit das Passwort eine längevon 10 Zeichen hat
+        //Die "while-Schleife" läuft bis i < 10 und somit das Passwort eine länge von 10 Zeichen hat
         while (i<10) {
-            int rand=randomize(0,127);
-            //Diese Bedingung unterbindet alle Ascii-Zeichen die nicht auf der Tastatur eingegeben werden können und die nur mit AltGr eingegeben werden können.
-            if (rand>32 && rand < 123 && rand!=64) {
-                char c = (char)rand;
-                ps+= Character.toString(c);
+            int rnd=randomize(0,127); //Aufruf der randomize-Funktion
+            //Diese Bedingung unterbindet alle Ascii-Zeichen die nicht auf der Tastatur eingegeben werden können oder die nur mit AltGr eingegeben werden können.
+            if (rnd>32 && rnd < 123 && rnd!=64 && rnd!=91 && rnd!=92 && rnd!=93) {
+                ps+= Character.toString((char)rnd);
                 i++;
             }
         };
-        System.out.println("Das neue Passwort ist: " + ps);
+        System.out.println("Das neue Passwort ist: " + ps); //Ausgabe auf der Console
     };
     //randomize Funktion die eine Zahl zwischen 0 und 127 zurück gibt
     static int randomize (int x,int y) {
         Random randomNum = new Random();
-        int rand = randomNum.nextInt((y-x)+1)+x;
-        return rand;
+        int rnd = randomNum.nextInt((y-x)+1)+x;
+        return rnd;
     }
 }
